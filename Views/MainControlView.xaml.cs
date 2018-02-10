9cbb06c61e2d84ba5574b54114f9361567fea82a
Andy Myers <jamyers@andy.local>
2018-02-10 13:47:47 -0500

    Fixed scrollview issue.  Textblock works with scrollview.

---
+++ Views/MainControlView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FontSamples.Views
+{
+    /// <summary>
+    /// Interaction logic for MainControlView.xaml
+    /// </summary>
+    public partial class MainControlView : UserControl
+    {
+        public MainControlView()
+        {
+            InitializeComponent();
+        }
+    }
+}
